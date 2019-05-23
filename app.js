9343181eccc372e3e16918855503372663713553
Trương Quốc Thịnh <quocthinhlk@gmail.com>
2019-05-23 21:36:57 +0700

    connect db

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello Nodejs');
+var mysql      = require('mysql');
+var db_config = {
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'qlbh',
+    port     : 8889
+    }
+var connection = mysql.createConnection(db_config);
+connection.connect();
+var sql = 'SELECT 1 + 1 AS solution';
+var fn = () => {
+    console.log('Hàm xử lý kết quả truy vấn');
+}
+
+connection.query(sql,fn);
+console.log('After querying...');
+
+connection.end();
